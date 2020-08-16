4491721f864d519739ac19ac6e79f1e80ce0b68e
sanjisand <61032413+sanjisand@users.noreply.github.com>
2020-08-15 23:44:15 -0500

    Updates

---
+++ precache-manifest.45959aab1ce48e6887e7013789e4bfe8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecadecdc92ca6a57a13abd3ee069874",
+    "url": "/perfil/index.html"
+  },
+  {
+    "revision": "5858973518bfd8d6ae0c",
+    "url": "/perfil/static/css/main.12ae120e.chunk.css"
+  },
+  {
+    "revision": "f1153aeff08d66a22f49",
+    "url": "/perfil/static/js/2.390d2b25.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/perfil/static/js/2.390d2b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5858973518bfd8d6ae0c",
+    "url": "/perfil/static/js/main.c727e359.chunk.js"
+  },
+  {
+    "revision": "5c269b8a9e29f4cdb801",
+    "url": "/perfil/static/js/runtime-main.9b8b9c5b.js"
+  },
+  {
+    "revision": "9bec958d1bdbaf4f03bdf23557b4122f",
+    "url": "/perfil/static/media/K2D-Regular.9bec958d.ttf"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/perfil/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "b3721ba3bde34e5b38b0e1523cccfd7f",
+    "url": "/perfil/static/media/PlayfairDisplay-Regular.b3721ba3.ttf"
+  },
+  {
+    "revision": "75b4247fdd3b97d0e3b8e07b115673c2",
+    "url": "/perfil/static/media/Raleway-Regular.75b4247f.ttf"
+  },
+  {
+    "revision": "66b0e223824fd123ab079b60da594ea7",
+    "url": "/perfil/static/media/Sacramento-Regular.66b0e223.ttf"
+  },
+  {
+    "revision": "cfea444bd152178f798332d0b7744fd1",
+    "url": "/perfil/static/media/bgSidebar.cfea444b.jpg"
+  },
+  {
+    "revision": "bf95ae8b7922a871fad54a5786e705a1",
+    "url": "/perfil/static/media/c1.bf95ae8b.jpg"
+  },
+  {
+    "revision": "5378add83f06f157ae1f0de90685059f",
+    "url": "/perfil/static/media/c2.5378add8.jpg"
+  },
+  {
+    "revision": "b1a07dc69930770e6890ae13c884ebda",
+    "url": "/perfil/static/media/c3.b1a07dc6.jpg"
+  },
+  {
+    "revision": "9486ff8596dff471674726a1783a5c37",
+    "url": "/perfil/static/media/c4.9486ff85.jpg"
+  },
+  {
+    "revision": "3e8becb9835dc9e1540532bf92c5683c",
+    "url": "/perfil/static/media/c5.3e8becb9.jpg"
+  },
+  {
+    "revision": "c5ea7e340685aa73f5a6e4006295a4af",
+    "url": "/perfil/static/media/c6.c5ea7e34.jpg"
+  },
+  {
+    "revision": "403bc24b64d4e243ced399b8d9aaa938",
+    "url": "/perfil/static/media/c7.403bc24b.jpg"
+  },
+  {
+    "revision": "ee7bffa0438d4e6a10098fd4ad26bfbd",
+    "url": "/perfil/static/media/c8.ee7bffa0.jpg"
+  },
+  {
+    "revision": "b9a9a7b8c56bdb84abfef18f77ab6362",
+    "url": "/perfil/static/media/cineteca.b9a9a7b8.jpg"
+  },
+  {
+    "revision": "d73a181f504bd4f2b3fdcefa243e68e2",
+    "url": "/perfil/static/media/crud.d73a181f.jpg"
+  },
+  {
+    "revision": "062a419e60552085c07a3fc5f1fcc169",
+    "url": "/perfil/static/media/fukuro.062a419e.jpg"
+  },
+  {
+    "revision": "100b4a2db9133f5ba1556d3f31cb9ba3",
+    "url": "/perfil/static/media/fukuro2.100b4a2d.jpg"
+  },
+  {
+    "revision": "2a2de9bebf76c339bae72cf57780317e",
+    "url": "/perfil/static/media/heroes.2a2de9be.jpg"
+  },
+  {
+    "revision": "3b7acebb989a329a3e6fc79632067865",
+    "url": "/perfil/static/media/login.3b7acebb.jpg"
+  },
+  {
+    "revision": "3a5b63f4b8258f9f6d7f4b867798d49c",
+    "url": "/perfil/static/media/perfil.3a5b63f4.jpg"
+  },
+  {
+    "revision": "9c04cdefe4a1a50d7fdba854c1484f91",
+    "url": "/perfil/static/media/perfil2.9c04cdef.jpg"
+  }
+]);
